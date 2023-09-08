f038ff5294ad8b979618ab48919cfaaf57d0bc50
Luka Krmpotic <luka.krmpotic@gmail.com>
2023-09-08 15:19:41 +0200

    get average temp at the init

--- love-o-meter/love-o-meter.ino
+++ love-o-meter/love-o-meter.ino
@@ -7,11 +7,13 @@ const int led_1 = 2;
 const int led_2 = 3;
 const int led_3 = 4;
 // loop delay
-const int dly_ms = 1000;
-// thresholds in Celsius
-const float cel_0 = 24.0;
-const float cel_1 = 25.0;
-const float cel_2 = 26.0;
+const int dly_ms = 500;
+// average temperature in celsius
+const int avg_cel_cycles = 5;
+float avg_cel = 0;
+float del_1 = 3;
+float del_2 = 4;
+float del_3 = 5;
 
 void setup() {
   Serial.begin(9600);
@@ -20,8 +22,10 @@ void setup() {
   pinMode(led_2, OUTPUT);
   pinMode(led_3, OUTPUT);
 
-  // LED test
+  // blink LEDs, and get starting temperature
   for (int i = 0; i < 5; i++) {
+    Serial.print(" LOVE-O-METER INIT");
+    // LED test
     digitalWrite(led_1, HIGH);
     digitalWrite(led_2, HIGH);
     digitalWrite(led_3, HIGH);
@@ -30,7 +34,12 @@ void setup() {
     digitalWrite(led_2, LOW);
     digitalWrite(led_3, LOW);
     delay(300);
+
+    int raw = analogRead(tmp36);
+    float cel = tmp36Cel(raw);
+    avg_cel += cel;
   }
+  avg_cel /= (float)avg_cel_cycles;
 }
 
 void loop() {
@@ -38,28 +47,31 @@ void loop() {
   float vol = tmp36Vol(raw);
   float cel = tmp36Cel(raw);
 
-  Serial.print(BUILD_GIT_SHORT);
-  Serial.print(" TMP36 raw: ");
+  Serial.print("git-");
+  Serial.print(BUILD_GIT_DIRTY);
+  Serial.print(" TMP36(");
   Serial.print(raw);
-  Serial.print(", vol: ");
+  Serial.print(") ");
   Serial.print(vol);
-  Serial.print(", degC: ");
+  Serial.print("V ");
   Serial.print(cel);
-  Serial.println();
+  Serial.print("C (");
+  Serial.print(cel-avg_cel);
+  Serial.println(")");
 
-  if (cel < cel_0) {
+  if (cel < avg_cel + del_1) {
     digitalWrite(led_1, LOW);
     digitalWrite(led_2, LOW);
     digitalWrite(led_3, LOW);
-  } else if (cel >= cel_0 && cel < cel_1) {
+  } else if (cel >= avg_cel + del_1 && cel < avg_cel + del_2) {
     digitalWrite(led_1, HIGH);
     digitalWrite(led_2, LOW);
     digitalWrite(led_3, LOW);
-  } else if (cel >= cel_1 && cel < cel_2) {
+  } else if (cel >= avg_cel + del_2 && cel < avg_cel + del_3) {
     digitalWrite(led_1, HIGH);
     digitalWrite(led_2, HIGH);
     digitalWrite(led_3, LOW);
-  } else if (cel >= cel_2) {
+  } else if (cel >= avg_cel + del_3) {
     digitalWrite(led_1, HIGH);
     digitalWrite(led_2, HIGH);
     digitalWrite(led_3, HIGH);
